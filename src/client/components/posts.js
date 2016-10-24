c81a754d58116b2657526e7c9d703fa4c03dfe77
anibalajt <anibalajt@gmail.com>
2016-10-24 09:40:15 -0500

    hover descripcion

--- src/client/components/posts.js
+++ src/client/components/posts.js
@@ -27,10 +27,11 @@ export default class Post extends React.Component {
     render () {
 
         let url = `/${this.props.url}${this.props.data.id}`
-
+        let resumen = ''
         let img = this.props.data['AgregaunaImagen']
         let name = '';
         let data = this.props.data
+
         let id = this.props.data.id
         let description = '';
         switch (parseInt(this.props.tipo)) {
@@ -41,19 +42,25 @@ export default class Post extends React.Component {
             case 1:
             name = FunctExtra.accentDecode(this.props.data['Título']);
             description = FunctExtra.accentDecode(this.props.data['Resumen']);
+            if(this.props.data.Resumen){
+                resumen = this.props.data['Resumen'].substring(0, 150);
+            }
             break;
             case 2:
             name = FunctExtra.accentDecode(this.props.data['Nombres']);
             description = FunctExtra.accentDecode(this.props.data['Resumen']);
             break;
         }
-
         return (
             <div>
                 <Link to={url}>
                     <figure className="targetPost">
                         <img src={`https://tupale.co/milfs/images/secure/?file=600/${img}`}/>
-                        <span style={this.state.background}>{name}</span>
+                        <div style={this.state.background} className="tP_description">
+                        <i className="plus i-plus"></i>
+                            <span className="titulo">{name}</span>
+                            <span className="description">{resumen}...</span>
+                        </div>
                     </figure>
                 </Link>
             </div>
